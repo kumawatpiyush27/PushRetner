9fa641b652a9fdcd654c56898ed6bd56b1999a49
kumawatpiyush27 <kumawatpiyush494@gmail.com>
2025-12-20 10:37:50 +0530

    Fix: Proper Vercel serverless function handler format

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // Vercel Serverless Function Handler
 const app = require('../backend/server');
 
-// Export the Express app as a serverless function
-module.exports = app;
+// Export as Vercel serverless function
+module.exports = (req, res) => {
+    return app(req, res);
+};
